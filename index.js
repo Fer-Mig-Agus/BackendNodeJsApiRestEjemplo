d1764aa82c040676585a9188700285a2052a5ff0
Fer-Mig-Agus <miguelfernandez.m.f.8998@gmail.com>
2023-12-05 18:39:13 -0300

    He cambiado la configuracion del index principal para que solo levante el servidor asi no de problemas la sincronizacion

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
-const servidor=require("./src/server/index");
-const BDD=require("./src/db/index");
+const servidor = require("./src/server/index");
+const BDD = require("./src/db/index");
 require("dotenv").config();
 
 
 const PORT = process.env.PORT || 3001;
 
-BDD.sync({ force: false }).then(() => {
-    servidor.listen(PORT, () => {
-        console.log("server listening on PORT: ", PORT);
-    })
-})
+//SINCRONIZACION CON LA BDD
 
+// BDD.sync({ force: false }).then(() => {
+//     servidor.listen(PORT, () => {
+//         console.log("server listening on PORT: ", PORT);
+//     })
+// })
+
+
+// SOLO LEVANTA EL SERVIDOR LOCAL
+
+servidor.listen(PORT, () => {
+    console.log("server listening on PORT: ", PORT);
+})
